01b3cbb7b40c7994d3bbe94fab4817ced1613639
ans2759 <ans2759@rit.edu>
2015-12-08 11:59:42 -0500

    State select set up

--- js/app.js
+++ js/app.js
@@ -1,51 +1,54 @@
 //$(document).foundation();
-    var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', []);
 
-    myApp.controller("ResponseController", ['$http', function ($http) {
-        var self = this;
-        var url = "http://people.rit.edu/dmgics/754/23/proxy.php";
-        self.states = [ "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN",
-            "KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY",
-            "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
+myApp.controller("ResponseController", ['$http', function ($http) {
+    var self = this;
+    var url = "http://people.rit.edu/dmgics/754/23/proxy.php";
+    self.states = [ "AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN",
+        "KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY",
+        "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"];
 
-        self.cities = [];
+    self.state = "NY";
+    self.cities = [];
+    //self.numCities = 0;
 
-        self.getCities = function (state) {
-            state = state || "NY";
-            /*$.ajax({
-                type: "GET", //GET is default
-                async: true, //default: true
-                cache: false, //default: false
-                url: url,
-                data: {path: "/Cities?state=" + state}, //necessary b/c we are sending through proxy
-                dataType: "xml",
-                success: self.handleResponse
-            });*/
-            $http.get( url, {
-                method: "GET",
-                params: {path: "/Cities?state=" + state},
-                responseType: "document"
-            }).success(self.handleResponse);
-        };
+    self.getCities = function (state) {
+        console.log("called: " + state);
+        self.state = state || "NY";
 
-        self.handleResponse = function (data) {
-            if ($(data).find("error").length !== 0) {
-                console.log("AJAX error");
-            }
-            else if ($(data).find("row").length === 0) {
-                self.cities = ["There are no cities in " + self.state];
-            }
-            else {
-                self.cities = [];
-                $("row", data).each(function () {
-                    self.addCity($(this).find("city").text());
-                });
-            }
-        };
+        $http.get( url, {
+            method: "GET",
+            params: {path: "/Cities?state=" + self.state},
+            responseType: "document"
+        }).success(self.handleResponse);
+    };
 
-        self.addCity = function (city) {
-            self.cities.push(city);
-        };
+    self.handleResponse = function (data) {
+        if ($(data).find("error").length !== 0) {
+            console.log("AJAX error");
+        }
+        else if ($(data).find("row").length === 0) {
+            self.cities = ["There are no cities in " + self.state];
+        }
+        else {
+            self.cities = [];
+            $("row", data).each(function () {
+                self.addCity($(this).find("city").text());
+            });
+        }
+    };
 
-        self.getCities();
-    }]);//end controller
+    self.addCity = function (city) {
+        self.cities.push(city);
+    };
+
+    self.getCities();
+
+    return self;
+}]);//end controller
+
+myApp.utils = (function() {
+    var me = {};
+
+    //$("[value='NY']").attr("selected", "selected"); // move to add
+}());
